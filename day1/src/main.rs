c6966f61b1b09100ca59325c078db43a5d17f097
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-12-02 17:17:15 +0100

    Add day 1 (1/2) solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufReader};
+use std::{fs::File, io::BufRead};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input")?;
+    let lines: Vec<i32> = BufReader::new(file)
+        .lines()
+        .map(|line| {
+            line.map(|line_str| line_str.parse::<i32>().unwrap())
+                .unwrap()
+        })
+        .collect();
+
+    for i in 0..lines.len() {
+        for j in i + 1..lines.len() {
+            if lines[i] + lines[j] == 2020 {
+                println!("{} + {} = 2020", lines[i], lines[j]);
+                println!("{} * {} = {}", lines[i], lines[j], lines[i] * lines[j]);
+            }
+        }
+    }
+
+    Ok(())
 }
