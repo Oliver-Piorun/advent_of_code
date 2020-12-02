1cf1a49b292dc21b21cc50f64bdad4a1cce55fd5
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-12-02 15:52:53 +0100

    Initial commit

---
+++ day1/target/debug/deps/day1-1b8218f746b44965.d
@@ -0,0 +1,5 @@
+c:\Users\opiorun\Desktop\aoc2020\day1\target\debug\deps\day1-1b8218f746b44965.rmeta: src\main.rs
+
+c:\Users\opiorun\Desktop\aoc2020\day1\target\debug\deps\day1-1b8218f746b44965.d: src\main.rs
+
+src\main.rs:
---
+++ day1/target/debug/deps/day1-d2fb9c56f76f4724.d
@@ -0,0 +1,5 @@
+c:\Users\opiorun\Desktop\aoc2020\day1\target\debug\deps\day1-d2fb9c56f76f4724.rmeta: src\main.rs
+
+c:\Users\opiorun\Desktop\aoc2020\day1\target\debug\deps\day1-d2fb9c56f76f4724.d: src\main.rs
+
+src\main.rs:
