39406f6cc4c511fecc7b9c6235c5c8a13a38299e
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-12-08 21:49:37 +0100

    Add day 6 (1/2) solution

---
+++ day6_1/src/main.rs
@@ -0,0 +1,25 @@
+use std::{collections::HashSet, error::Error, fs::read_to_string};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = read_to_string("input")?;
+    let groups: Vec<&str> = input.split("\r\n\r\n").collect();
+
+    let mut num_yes = 0;
+
+    for group in groups {
+        let mut characters = HashSet::new();
+
+        for character in group.chars() {
+            // Skip carriage return (CR) and line feed (LF)
+            if character != '\r' && character != '\n' {
+                characters.insert(character);
+            }
+        }
+
+        num_yes += characters.len();
+    }
+
+    println!("{}", num_yes);
+
+    Ok(())
+}
