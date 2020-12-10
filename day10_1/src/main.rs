40fe2ca433d4808babb1623e9bb45658828463b2
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-12-10 16:08:15 +0100

    Add day 10 (1/2) solution

---
+++ day10_1/src/main.rs
@@ -0,0 +1,42 @@
+use std::{error::Error, io::BufReader};
+use std::{fs::File, io::BufRead};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input")?;
+    let mut numbers: Vec<u8> = BufReader::new(file)
+        .lines()
+        .map(|line| line.unwrap().parse::<u8>().unwrap())
+        .collect();
+    numbers.sort_unstable();
+
+    let mut num_one_jolt_difference = 0;
+    let mut num_three_jolt_difference = 0;
+
+    for (i, number) in numbers.iter().enumerate() {
+        let jolt_difference;
+
+        if i == 0 {
+            jolt_difference = *number;
+        } else {
+            jolt_difference = number - numbers[i - 1];
+        }
+
+        if jolt_difference == 1 {
+            num_one_jolt_difference += 1;
+        } else if jolt_difference == 3 {
+            num_three_jolt_difference += 1;
+        }
+    }
+
+    // Last adapter to device difference
+    num_three_jolt_difference += 1;
+
+    println!(
+        "{} * {} = {}",
+        num_one_jolt_difference,
+        num_three_jolt_difference,
+        num_three_jolt_difference * num_one_jolt_difference
+    );
+
+    Ok(())
+}
