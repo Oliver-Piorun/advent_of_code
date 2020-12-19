93415cf427f892802b7266bebd77876b6dea8f8a
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2020-12-19 16:56:53 +0100

    Improve day 13 (2/2) solution

--- day13_2/src/main.rs
+++ day13_2/src/main.rs
@@ -24,29 +24,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let earliest_timestamp_threshold = 100_000_000_000_000;
     let first_bus = buses[0];
-    let mut current_timestamp = get_earliest_timestamp(earliest_timestamp_threshold, first_bus.0);
+    let first_bus_timestamp = first_bus.0;
+    let mut current_timestamp =
+        get_earliest_timestamp(earliest_timestamp_threshold, first_bus_timestamp);
     let mut timestamp_step = first_bus.0;
-    let mut bus_id_index = 1;
+    let mut bus_index = 1;
 
     loop {
-        let bus = buses[bus_id_index];
-        let bus_id = bus.0;
+        let bus = buses[bus_index];
+        let bus_timestamp = bus.0;
+        let bus_timestamp_offet = bus.1;
         let target_timestamp = current_timestamp + bus.1 as i64;
-        let calculated_bus_timestamp = get_earliest_timestamp(target_timestamp, bus_id);
+        let calculated_bus_timestamp = get_earliest_timestamp(target_timestamp, bus_timestamp);
 
         // Check if the calculated bus timestamp is fitting the current timestamp
-        if calculated_bus_timestamp - bus.1 as i64 == current_timestamp {
-            // Check if we processed all bus ids
-            if bus_id_index + 1 >= buses.len() {
+        if calculated_bus_timestamp - bus_timestamp_offet as i64 == current_timestamp {
+            // Check if we processed all buses
+            if bus_index + 1 >= buses.len() {
                 println!("{}", current_timestamp);
                 break;
             }
 
-            // Update the timestamp step based on the bus id
-            timestamp_step *= bus_id;
+            // Update the timestamp step based on the bus timestamp
+            // Buses which meet at timestamp 7 and 9 also meet at timestamp 7 * 9 = 63
+            // Note that we are dealing with prime numbers, so the least common multiple (LCM) is just p1 * p2
+            timestamp_step *= bus_timestamp;
 
             // Proceed with the next bus
-            bus_id_index += 1;
+            bus_index += 1;
         }
 
         current_timestamp += timestamp_step;
@@ -55,8 +60,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn get_earliest_timestamp(timestamp: i64, bus_id: i64) -> i64 {
-    let multiplier = (timestamp as f64 / bus_id as f64).ceil() as i64;
+fn get_earliest_timestamp(target_timestamp: i64, bus_timestamp: i64) -> i64 {
+    let multiplier = (target_timestamp as f64 / bus_timestamp as f64).ceil() as i64;
 
-    bus_id * multiplier
+    bus_timestamp * multiplier
 }
