78992a63d77881f7742a2ef29d37b480e1f10fe4
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2021-12-04 02:40:19 +0100

    Cleanup solutions

--- 2021/day03/src/main.rs
+++ 2021/day03/src/main.rs
@@ -1,8 +1,13 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader, Lines},
-    str::FromStr,
+    io::{self, BufRead, BufReader},
 };
+
+#[derive(PartialEq, Copy, Clone)]
+enum RatingType {
+    OxygenGenerator,
+    CO2Scrubber,
+}
 
 fn main() -> io::Result<()> {
     // Open the file
@@ -21,120 +26,116 @@ fn main() -> io::Result<()> {
 }
 
 fn part1(lines: &[String]) {
-    let mut num_unset_bits = 0;
-    let mut num_set_bits = 0;
+    let num_horizontal_bits = lines[0].len();
 
     let mut gamma_rate = 0;
 
-    for bit_index in 0..=11 {
-        // Take a look at each line
-        for line in lines {
-            let bit = line.chars().nth(bit_index).unwrap();
+    // Take a look at each horizontal bit
+    for horizontal_bit_index in 0..num_horizontal_bits {
+        // Get the most common bit
+        let most_common_bit = get_most_common_vertical_bit(lines, horizontal_bit_index);
 
-            if bit == '0' {
-                num_unset_bits += 1;
-            } else if bit == '1' {
-                num_set_bits += 1;
-            }
+        if most_common_bit == '0' {
+            // We do not have to set a bit here
+            continue;
         }
 
-        if num_set_bits > num_unset_bits {
-            gamma_rate |= 1 << 11 - bit_index;
-        }
-
-        num_unset_bits = 0;
-        num_set_bits = 0;
+        // Set the bit at the horizontal bit index
+        gamma_rate |= 1 << (num_horizontal_bits - 1 - horizontal_bit_index);
     }
 
-    let epsilon_rate = !gamma_rate & 0b00000000000000000000111111111111;
+    let bit_mask = !(!0 << num_horizontal_bits);
 
-    println!("{gamma_rate}");
-    println!("{epsilon_rate}");
+    // Invert the gamma rate and clear all not relevant bits with a bit mask
+    let epsilon_rate = !gamma_rate & bit_mask;
 
     println!("part1: {}", gamma_rate * epsilon_rate);
 }
 
 fn part2(lines: &[String]) {
-    let mut oxygen_vec: Vec<String> = lines.to_vec();
-    let mut co2_vec: Vec<String> = lines.to_vec();
+    // Get the oxygen generator rating
+    let oxygen_generator_rating = get_rating(lines, RatingType::OxygenGenerator);
 
-    let mut bit_index = 0;
+    // Get the CO2 scrubber rating
+    let co2_scrubber_rating = get_rating(lines, RatingType::CO2Scrubber);
 
-    while oxygen_vec.len() != 1 {
-        println!("oxy len {}", oxygen_vec.len());
-        oxygen_vec = oxygen(&oxygen_vec, bit_index);
-        bit_index += 1;
-    }
-
-    bit_index = 0;
-
-    while co2_vec.len() != 1 {
-        println!("co2 len {}", co2_vec.len());
-        co2_vec = co2(&co2_vec, bit_index);
-        bit_index += 1;
-    }
-
-    println!("oxy {}", oxygen_vec.len());
-    println!("co2 {}", co2_vec.len());
-
-    println!("part2: {}", oxygen_vec.first().unwrap());
-    println!("part2: {}", co2_vec.first().unwrap());
+    println!("part2: {}", oxygen_generator_rating * co2_scrubber_rating);
 }
 
-fn oxygen(lines: &[String], bit_index: usize) -> Vec<String> {
+fn get_most_common_vertical_bit(lines: &[String], horizontal_bit_index: usize) -> char {
+    let num_vertical_bits = lines.len() as i32;
+
     let mut num_unset_bits = 0;
     let mut num_set_bits = 0;
 
-    for line in lines {
-        let bit = line.chars().nth(bit_index).unwrap();
+    // Take a look at each vertical bit
+    for (vertical_bit_index, line) in lines.iter().enumerate() {
+        // Get the horizontal bit
+        let horizontal_bit = line.chars().nth(horizontal_bit_index).unwrap();
 
-        if bit == '0' {
-            num_unset_bits += 1;
-        } else if bit == '1' {
-            num_set_bits += 1;
+        match horizontal_bit {
+            '0' => num_unset_bits += 1,
+            '1' => num_set_bits += 1,
+            _ => panic!("Invalid horizontal bit!"),
+        };
+
+        let num_remaining_vertical_bits = num_vertical_bits - vertical_bit_index as i32 - 1;
+
+        if num_unset_bits > num_set_bits + num_remaining_vertical_bits {
+            // Check if enough bits are unset to call them more common
+            return '0';
+        } else if num_set_bits >= num_unset_bits + num_remaining_vertical_bits {
+            // Check if enough bits are set to call them more common
+            return '1';
         }
     }
 
-    let mut c = '\0';
-
-    if num_set_bits >= num_unset_bits {
-        c = '1';
-    } else {
-        c = '0';
-    }
-
-    let mut v = lines.to_vec();
-
-    v.retain(|line| line.chars().nth(bit_index).unwrap() == c);
-
-    v
+    panic!("We should never land here!")
 }
 
-fn co2(lines: &[String], bit_index: usize) -> Vec<String> {
-    let mut num_unset_bits = 0;
-    let mut num_set_bits = 0;
+fn get_rating(lines: &[String], rating_type: RatingType) -> i32 {
+    let mut rating_vec: Vec<String> = lines.to_vec();
+    let mut horizontal_bit_index = 0;
 
-    for line in lines {
-        let bit = line.chars().nth(bit_index).unwrap();
+    while rating_vec.len() != 1 {
+        // Get the most common vertical bit
+        let most_common_vertical_bit =
+            get_most_common_vertical_bit(&rating_vec, horizontal_bit_index);
 
-        if bit == '0' {
-            num_unset_bits += 1;
-        } else if bit == '1' {
-            num_set_bits += 1;
-        }
+        // Filter by most common vertical bit and rating type
+        filter(
+            &mut rating_vec,
+            horizontal_bit_index,
+            most_common_vertical_bit,
+            rating_type,
+        );
+
+        // Increase horizontal bit index
+        horizontal_bit_index += 1;
     }
 
-    let mut c = '\0';
+    // Convert binary string to an integer
+    i32::from_str_radix(&rating_vec[0], 2).unwrap()
+}
 
-    if num_set_bits >= num_unset_bits {
-        c = '0';
-    } else {
-        c = '1';
-    }
+fn filter(
+    lines: &mut Vec<String>,
+    horizontal_bit_index: usize,
+    most_common_vertical_bit: char,
+    rating_type: RatingType,
+) {
+    let horizontal_bit_to_retain = match most_common_vertical_bit {
+        '0' => match rating_type {
+            RatingType::OxygenGenerator => '0',
+            RatingType::CO2Scrubber => '1',
+        },
+        '1' => match rating_type {
+            RatingType::OxygenGenerator => '1',
+            RatingType::CO2Scrubber => '0',
+        },
+        _ => panic!("Invalid most common vertical bit!"),
+    };
 
-    let mut v = lines.to_vec();
-
-    v.retain(|line| line.chars().nth(bit_index).unwrap() == c);
-
-    v
+    lines
+        .retain(|line| line.chars().nth(horizontal_bit_index).unwrap() == horizontal_bit_to_retain);
 }
