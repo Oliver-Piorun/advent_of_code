ff021ef3ece8ce21c0220b60c1bbd48ad083015f
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2022-12-06 18:01:23 +0100

    20x performance increase for day 2 solutions

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -2,87 +2,72 @@
 #![feature(test)]
 extern crate test;
 
-#[derive(Copy, Clone, PartialEq, PartialOrd)]
-enum Shape {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3,
-}
-
-enum Outcome {
-    Lose = 1,
-    Draw = 2,
-    Win = 3,
-}
-
-// The idiotmatic way would be to implement the "FromStr" trait here but we keep it a bit simpler/shorter
-fn to_shape(str: &str) -> Shape {
-    match str {
-        "A" | "X" => Shape::Rock,
-        "B" | "Y" => Shape::Paper,
-        "C" | "Z" => Shape::Scissors,
-        _ => panic!("Unexpected shape!"),
-    }
-}
-
-fn to_outcome(str: &str) -> Outcome {
-    match str {
-        "X" => Outcome::Lose,
-        "Y" => Outcome::Draw,
-        "Z" => Outcome::Win,
-        _ => panic!("Unexpected outcome!"),
-    }
-}
-
 fn main() {
     part1();
     part2();
 }
 
 fn part1() -> i32 {
-    let input = include_str!("../input");
+    let input = include_bytes!("../input");
     let mut total_score = 0;
 
-    for line in input.lines() {
-        let split = line.split_whitespace().collect::<Vec<&str>>();
-        let op_shape = to_shape(split.first().unwrap());
-        let my_shape = to_shape(split.get(1).unwrap());
+    // Align 'A' and 'X'
+    // 'X' (88) - 'A' (65) = 23
+    let alignment = b'X' - b'A';
+
+    for i in (0..input.len()).step_by(4) {
+        let op_shape = input[i];
+        let my_shape = input[i + 2];
 
         let score = match (op_shape, my_shape) {
-            (Shape::Rock, Shape::Scissors) => 0,
-            (Shape::Scissors, Shape::Rock) => 6,
+            // Rock, Scissors
+            (b'A', b'Z') => 0,
+            // Scissors, Rock
+            (b'C', b'X') => 6,
             _ => {
-                if op_shape > my_shape {
+                if op_shape > my_shape - alignment {
+                    // Lose
                     0
-                } else if op_shape < my_shape {
+                } else if op_shape < my_shape - alignment {
+                    // Win
                     6
                 } else {
+                    // Draw
                     3
                 }
             }
         };
 
-        total_score += my_shape as i32 + score;
+        // Align my_shape and 'X'
+        // 'X' (88) - 'X' (88) = 0
+        // 'Y' (89) - 'X' (88) = 1
+        // 'Z' (90) - 'X' (88) = 2
+        total_score += (my_shape - b'X' + 1) as i32 + score;
     }
 
     total_score
 }
 
 fn part2() -> i32 {
-    let input = include_str!("../input");
+    let input = include_bytes!("../input");
     let mut total_score = 0;
 
-    for line in input.lines() {
-        let split = line.split_whitespace().collect::<Vec<&str>>();
-        let op_shape = to_shape(split.first().unwrap());
-        let exp_outcome = to_outcome(split.get(1).unwrap());
+    for i in (0..input.len()).step_by(4) {
+        let op_shape = input[i];
+        let exp_outcome = input[i + 2];
 
         let score = match (op_shape, exp_outcome) {
-            (Shape::Rock, Outcome::Lose) => Shape::Scissors as i32,
-            (Shape::Scissors, Outcome::Win) => Shape::Rock as i32 + 6,
-            (_, Outcome::Lose) => op_shape as i32 - 1,
-            (_, Outcome::Draw) => op_shape as i32 + 3,
-            (_, Outcome::Win) => op_shape as i32 + 1 + 6,
+            // Rock, Lose => Scissors
+            (b'A', b'X') => 3,
+            // Scissors, Win => Rock
+            (b'C', b'Z') => 1 + 6,
+            // Any, Lose
+            (_, b'X') => (op_shape - b'A' + 1) as i32 - 1,
+            // Any, Draw
+            (_, b'Y') => (op_shape - b'A' + 1) as i32 + 3,
+            // Any, Win
+            (_, b'Z') => (op_shape - b'A' + 1) as i32 + 1 + 6,
+            _ => panic!("We should never land here!"),
         };
 
         total_score += score;
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ year    day      part 1       part 2
 2020         <Not benchmarked>
 2021         <Not benchmarked>
 2022    day01      8.39 μs      9.85 μs
-2022    day02    213.26 μs    222.73 μs
+2022    day02      9.14 μs     11.99 μs
 2022    day03     29.03 μs     44.77 μs
 2022    day04      9.42 μs      9.05 μs
 2022    day05     12.95 μs     41.99 μs
