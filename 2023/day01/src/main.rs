f33bd8f376787ab19a08e191d6ab009ec7b7418a
Oliver Piorun <Oliver.Piorun@ruhr-uni-bochum.de>
2023-12-02 02:29:18 +0100

    Merge part1 and part2

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -10,8 +10,20 @@ fn main() {
     part2();
 }
 
-#[inline(always)]
 fn part1() -> u32 {
+    get_calibration_value_sum(false)
+}
+
+fn part2() -> u32 {
+    get_calibration_value_sum(true)
+}
+
+#[inline(always)]
+fn get_calibration_value_sum(consider_spelled_out_digits: bool) -> u32 {
+    const SPELLED_OUT_DIGITS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
     let input = include_bytes!("../input");
     let mut input_index = 0;
 
@@ -37,6 +49,19 @@ fn part1() -> u32 {
             if input[line_index] >= b'1' && input[line_index] <= b'9' {
                 first_digit_char = input[line_index];
                 break;
+            } else if consider_spelled_out_digits {
+                // The maximum number of letters for a spelled out digit is 5
+                let to_line_index = min(line_index + 4, input.len() - 1);
+                let substring = std::str::from_utf8(&input[line_index..=to_line_index]).unwrap();
+
+                let matched_spelled_out_digit = SPELLED_OUT_DIGITS
+                    .iter()
+                    .find(|&&spelled_out_digit| substring.starts_with(spelled_out_digit));
+
+                if let Some(matched_spelled_out_digit) = matched_spelled_out_digit {
+                    first_digit_char = map_to_digit_char(matched_spelled_out_digit);
+                    break;
+                }
             }
 
             line_index += 1;
@@ -48,6 +73,19 @@ fn part1() -> u32 {
             if input[line_index] >= b'1' && input[line_index] <= b'9' {
                 last_digit_char = input[line_index];
                 break;
+            } else if consider_spelled_out_digits {
+                // The maximum number of letters for a spelled out digit is 5
+                let from_line_index = max(0, line_index as isize - 4) as usize;
+                let substring = std::str::from_utf8(&input[from_line_index..=line_index]).unwrap();
+
+                let matched_spelled_out_digit = SPELLED_OUT_DIGITS
+                    .iter()
+                    .find(|&&spelled_out_digit| substring.ends_with(spelled_out_digit));
+
+                if let Some(matched_spelled_out_digit) = matched_spelled_out_digit {
+                    last_digit_char = map_to_digit_char(matched_spelled_out_digit);
+                    break;
+                }
             }
 
             line_index -= 1;
@@ -68,102 +106,17 @@ fn part1() -> u32 {
 }
 
 #[inline(always)]
-fn part2() -> u32 {
-    const DIGITS_AS_STR: [&str; 9] = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-
-    let input = include_bytes!("../input");
-    let mut input_index = 0;
-
-    let mut calibration_value_sum = 0;
-
-    loop {
-        let mut line_index = input_index;
-
-        let mut first_digit = 0;
-        let last_digit;
-
-        let line_break_index = memchr(b'\n', &input[line_index..]);
-
-        let end_index;
-
-        if let Some(line_break_index) = line_break_index {
-            end_index = line_index + line_break_index;
-        } else {
-            end_index = input.len();
-        }
-
-        while line_index < end_index {
-            if input[line_index] >= b'1' && input[line_index] <= b'9' {
-                first_digit = input[line_index] - b'0';
-                break;
-            } else {
-                let to_line_index = min(line_index + 4, input.len() - 1);
-                let substring = std::str::from_utf8(&input[line_index..=to_line_index]).unwrap();
-
-                let matched_digit_as_str = DIGITS_AS_STR
-                    .iter()
-                    .find(|&&digit_as_str| substring.starts_with(digit_as_str));
-
-                if let Some(matched_digit_as_str) = matched_digit_as_str {
-                    first_digit = map_to_digit(matched_digit_as_str);
-                    break;
-                }
-            }
-
-            line_index += 1;
-        }
-
-        line_index = end_index - 1;
-
-        loop {
-            if input[line_index] >= b'1' && input[line_index] <= b'9' {
-                last_digit = input[line_index] - b'0';
-                break;
-            } else {
-                let from_line_index = max(0, line_index as isize - 4) as usize;
-                let substring = std::str::from_utf8(&input[from_line_index..=line_index]).unwrap();
-
-                let matched_digit_as_str = DIGITS_AS_STR
-                    .iter()
-                    .find(|&&digit_as_str| substring.ends_with(digit_as_str));
-
-                if let Some(matched_digit_as_str) = matched_digit_as_str {
-                    last_digit = map_to_digit(matched_digit_as_str);
-                    break;
-                }
-            }
-
-            line_index -= 1;
-        }
-
-        calibration_value_sum += (first_digit * 10 + last_digit) as u32;
-
-        line_index = end_index;
-
-        if line_index == input.len() {
-            break;
-        }
-
-        input_index = line_index + 1;
-    }
-
-    calibration_value_sum
-}
-
-#[inline(always)]
-fn map_to_digit(digit_as_str: &str) -> u8 {
-    match digit_as_str {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
+fn map_to_digit_char(spelled_out_digit: &str) -> u8 {
+    match spelled_out_digit {
+        "one" => b'1',
+        "two" => b'2',
+        "three" => b'3',
+        "four" => b'4',
+        "five" => b'5',
+        "six" => b'6',
+        "seven" => b'7',
+        "eight" => b'8',
+        "nine" => b'9',
         _ => panic!(),
     }
 }
